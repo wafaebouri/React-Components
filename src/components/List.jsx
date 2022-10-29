c8c507e1befc43ef6f4a3f4cbac585211f23bbaa
wafaebouri <116091410+wafaebouri@users.noreply.github.com>
2022-10-28 22:28:11 -0400

    Initial commit

---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Lamb Meat</li>
+      <li>Mango</li>
+      <li>Noodles</li>
+    </ul>
+  );
+}
+
+export default List;
